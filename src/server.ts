70e7f137249683eb1461e3f9b1710755f9e4fd17
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-06-20 17:29:06 +0100

    Added typescript typing.

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: express.Request, res: express.Response) => {
     const {image_url} = req.query;
     
     if(!image_url) {
@@ -52,7 +52,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: express.Request, res: express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
